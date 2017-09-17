d56283d742a1a2369082a283c0ebba86fed3a861
Stu <supadupastu@gmail.com>
2017-09-17 13:24:04 -0700

    rename app, remove analog pin stuff

--- picoproapp/src/main/java/com/example/androidthings/myproject/MovementApp.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/MovementApp.java
@@ -8,8 +8,8 @@ import com.google.android.things.contrib.driver.mma8451q.Mma8451Q;
 import com.google.android.things.pio.Gpio;
 
 /**
- * HW3 Template
- * Created by bjoern on 9/12/17.
+ * HW3 Movement
+ * Based on HW3 Template by bjoern on 9/12/17.
  * Wiring:
  * USB-Serial Cable:
  *   GND to GND on IDD Hat
@@ -20,11 +20,9 @@ import com.google.android.things.pio.Gpio;
  *   GND to GND on IDD Hat
  *   SCL to SCL on IDD Hat
  *   SDA to SDA on IDD Hat
- * Analog sensors:
- *   Middle of voltage divider to Analog A0..A3 on IDD Hat
  */
 
-public class Hw3TemplateApp extends SimplePicoPro {
+public class MovementApp extends SimplePicoPro {
 
     Mma8451Q accelerometer;
 
@@ -33,43 +31,26 @@ public class Hw3TemplateApp extends SimplePicoPro {
 
 
     float[] xyz = {0.f,0.f,0.f}; //store X,Y,Z acceleration of MMA8451 accelerometer here [units: G]
-    float a0,a1,a2,a3; //store analog readings from ADS1015 ADC here [units: V]
 
     public void setup() {
         // Initialize the serial port for communicating to a PC
         uartInit(UART6,9600);
-        //uartInit(UART6,38400);
-
-        // Initialize the Analog-to-Digital converter on the HAT
-        analogInit(); //need to call this first before calling analogRead()
 
         // Initialize the MMQ8451 Accelerometer
-
         try {
             accelerometer = new Mma8451Q("I2C1");
             accelerometer.setMode(Mma8451Q.MODE_ACTIVE);
         } catch (IOException e) {
-            Log.e("HW3Template","setup",e);
+            Log.e("Movement","setup",e);
         }
 
-
-        //set edge triggers for fire and reload buttons
+        // set edge triggers for fire and reload buttons
         setEdgeTrigger(fireButton, Gpio.EDGE_FALLING);
         setEdgeTrigger(reloadButton, Gpio.EDGE_FALLING);
     }
 
     public void loop() {
-        // read all analog channels and print to UART
-        a0 = analogRead(A0);
-        a1 = analogRead(A1);
-        a2 = analogRead(A2);
-        a3 = analogRead(A3);
-        //println(UART6,"A0: "+a0+"   A1: "+a1+"   A2: "+a2+"   A3: "+a3); // this goes to the Serial port
-        //println("A0: "+a0+"   A1: "+a1+"   A2: "+a2+"   A3: "+a3); // this goes to the Android Monitor in Android Studio
-
-
         // read I2C accelerometer and print to UART
-
         try {
             xyz = accelerometer.readSample();
             //println(UART6,"X: "+xyz[0]+"   Y: "+xyz[1]+"   Z: "+xyz[2]);
@@ -83,25 +64,22 @@ public class Hw3TemplateApp extends SimplePicoPro {
             //println(UART6,xyz[0]+"\t"+xyz[1]+"\t"+xyz[2]); // this goes to the Serial port
 
         } catch (IOException e) {
-            Log.e("HW3Template","loop",e);
+            Log.e("Movement","loop",e);
         }
 
-
-
         delay(10);
-
     }
 
     @Override
     void digitalEdgeEvent(Gpio pin, boolean value) {
-        //println("digitalEdgeEvent"+pin+", "+value);
-
         // when button is pressed down for pull up resistor switches
+        // Fire event
         if(pin==fireButton && value==LOW) {
             println(UART6, "FIRE");
             println("fire");
         }
-        //when 39 goes from HIGH to HIGH
+        // when button is pressed down for pull up resistor switches
+        // reload event
         else if (pin==reloadButton && value==LOW) {
             println(UART6, "RELOAD");
             println("reload");
